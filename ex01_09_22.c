a72036c283f12567eb8847950ec7b49ed363075e
WilliamXnl <81707342+WilliamXnl@users.noreply.github.com>
2022-09-01 11:48:14 -0300

    Add files via upload

---
+++ ex01_09_22.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+typedef struct{
+
+    int id;
+    char modelo;
+    tMotor mot;
+    tSistFreios fr;
+    tPneu pn[5];
+
+}
+
+typedef struct{
+
+    float tamanho;
+    int valvulas;
+    char fabricante;
+
+
+
+} tMotor;
+
+typedef struct{
+
+    int ABS;
+    char numSerie;
+
+} tSistFreios;
+
+typedef struct{
+
+    
+    float pressao;
+
+}
+
+int main(void){
+
+
+
+
+
+
+
+
+
+
+}
